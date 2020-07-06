ce8f3061686e04db6e268db29499a4bd6bb2ae41
Zoilo Granda <zgranda@gmail.com>
2020-07-06 08:56:08 -0400

    7. Sending Responses

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
  console.log(req);
+ res.setHeader('Content-Type','text/html')
+ res.write('<html>')
+ res.write('<head><title>My page</title></head>')
+ res.write('</html>')
+ res.end()
  process.exit()
 })
 
