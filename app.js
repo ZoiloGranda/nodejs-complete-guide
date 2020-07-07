74cf32705f11b90ece4c46bc88287c7b1203f754
Zoilo Granda <zgranda@gmail.com>
2020-07-07 08:39:40 -0400

    9. Routing Requests

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
- console.log(req);
+ const url = req.url
+ if (url === '/') {
+  res.write('<html>')
+  res.write('<head><title>Enter Message</title></head>')
+  res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+  res.write('</html>')
+  return res.end()  
+ }
  res.setHeader('Content-Type','text/html')
  res.write('<html>')
  res.write('<head><title>My page</title></head>')
+ res.write('<body><h1>My page</h1></body>')
  res.write('</html>')
  res.end()
- process.exit()
+ // process.exit()
 })
 
 server.listen(3000)
