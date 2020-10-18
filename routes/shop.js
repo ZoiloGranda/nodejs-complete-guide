0afb4a343ad0c58b70b99d6c55e6bc848753ad02
Zoilo Granda <zgranda@gmail.com>
2020-10-18 15:47:13 -0400

    14. Working with EJS

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const path = require('path');
-const rootDir = require('../util/path')
+const express = require("express");
+const path = require("path");
+const rootDir = require("../util/path");
 
-const router = express.Router()
-const adminData = require('./admin')
+const router = express.Router();
+const adminData = require("./admin");
 
-router.get('/',(req, res, next)=>{
- const products = adminData.products
- res.render('shop', {prods:products, pageTitle: 'Shop', path:'/'})
-})
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", { 
+   prods: products,
+   pageTitle: "Shop",
+   path: "/",
+   hasProducts: products.length > 0,
+   activeShop: true,
+   productCSS: true
+  });
+});
 
 module.exports = router;
