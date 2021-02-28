f614bda6fd4a366804eb81aa668246ae223705e8
Zoilo Granda <zgranda@gmail.com>
2021-02-28 08:39:54 -0400

    12. Fetching a Single Product with  Condition

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,24 +3,24 @@ const Cart = require('../models/cart')
 
 exports.getProducts = (req, res, next) => {
  Product.fetchAll()
- .then(([rows]) => {
-  res.render('shop/product-list', {
-   prods: rows,
-   pageTitle: 'All Products',
-   path: '/products'
-  });
- }).catch(err => console.log(err))
+  .then(([rows]) => {
+   res.render('shop/product-list', {
+    prods: rows,
+    pageTitle: 'All Products',
+    path: '/products'
+   });
+  }).catch(err => console.log(err))
 };
 
 exports.getProduct = (req, res, next) => {
  const prodId = req.params.productId
- Product.findById(prodId, product => {
+ Product.findById(prodId).then(([product]) => {
   res.render('shop/product-detail', {
-   product,
+   product: product[0],
    pageTitle: product.title,
    path: '/products'
   })
- })
+ }).catch(err => console.log(err))
 };
 
 exports.getIndex = (req, res, next) => {
