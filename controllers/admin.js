17572a6866120e30fce869889c79a2a3c04dc1f2
Zoilo Granda <zgranda@gmail.com>
2021-03-16 10:40:06 -0400

    12. Deleting Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,7 +18,10 @@ exports.postAddProduct = (req, res, next) => {
    price,
    imageUrl,
    description
-  }).then(result => console.log(result))
+  }).then(result => {
+   console.log(result)
+   res.redirect('/admin/products')
+  })
   .catch(err => console.log(err))
 };
 
@@ -78,6 +81,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
  const prodId = req.body.productId
- Product.deleteById(prodId)
- res.redirect('/admin/products')
+ Product.findByPk(prodId)
+  .then(product => {
+   return product.destroy()
+  })
+  .then(result => {
+   console.log('DELETED PRODUCT');
+   res.redirect('/admin/products')
+  })
+  .catch(err => console.log(err))
 }
