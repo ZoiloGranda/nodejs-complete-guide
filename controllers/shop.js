658cdebf7f8747b8f8b0851ce9af140671cbd54a
Zoilo Granda <zgranda@gmail.com>
2021-04-07 09:42:53 -0400

    25. Resetting the Cart & Outputting Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -99,9 +99,11 @@ exports.postCart = (req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
+ let fetchedCart;
  req.user
   .getCart()
   .then(cart => {
+   fetchedCart = cart
    return cart.getProducts()
   }).then(products => {
    return req.user.createOrder()
@@ -113,6 +115,8 @@ exports.postOrder = (req, res, next) => {
       return product
      }))
     }).then(result => {
+     return fetchedCart.setProducts(null)
+    }).then(result => {
      res.redirect('/orders')
     })
     .catch(err => console.log(err))
@@ -120,15 +124,13 @@ exports.postOrder = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
- res.render('shop/orders', {
-  path: '/orders',
-  pageTitle: 'Your Orders'
- });
-};
-
-exports.getCheckout = (req, res, next) => {
- res.render('shop/checkout', {
-  path: '/checkout',
-  pageTitle: 'Checkout'
- });
+ req.user.getOrders({ include: ['products'] })
+  .then(orders => {
+   res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders',
+    orders: orders
+   });
+  })
+  .catch(err => console.log(err))
 };
